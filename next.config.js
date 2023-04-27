10a27ae23833982f342a64ca58619dc6d6804531
jose <joseandrescolmenares02@gmail.com>
2023-04-26 22:47:25 -0300

    fix: working environment configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
+const  EslintPlugin = require('eslint-webpack-plugin')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  webpack: (config, env) => {
+    if(env.dev){
+      config.plugins.push(new EslintPlugin({
+        extensions: ["js", "jsx", "ts", "tsx"],
+        files: ["pages","src"],
+        failOnError: false,
+      }))
+    }
+    return config
+  }
+
 }
 
 module.exports = nextConfig
